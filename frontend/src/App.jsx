c9c550a59cc304a98257459c5b32d0b80aeb9698
LalithKumarC2007 <lalithkumarc23@gmail.com>
2026-03-04 09:23:10 +0530

    Footer with quick links and contact us

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import "./App.css"
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Student Developer Platform 🚀
-      </h1>
+    <div className="min-h-screen flex flex-col bg-gray-100" style={{ width: "100%", margin: 0, padding: 0 }}>
+      <div className="flex-1 flex items-center justify-center">
+        <h1 className="text-3xl font-bold text-blue-600">
+          Student Developer Platform 🚀
+        </h1>
+      </div>
+
+      <Footer />
     </div>
   );
 }
